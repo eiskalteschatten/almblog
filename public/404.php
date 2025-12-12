98b93bb3e00dc4ea781a524aefda312b0b6caddc
Alex <alex@alexseifert.com>
2025-12-12 09:31:44 +0100

    Add the strict_types=1 directive

--- public/404.php
+++ public/404.php
@@ -1,5 +1,8 @@
 <?php
+declare(strict_types=1);
+
 http_response_code(404);
+
 require_once(__DIR__ . "/../src/bootstrap.php");
 
 use App\Config\AppConfig;
--- src/autoload.php
+++ src/autoload.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 spl_autoload_register(function ($class) {
     // Define namespace to directory mappings
     $prefixes = [
