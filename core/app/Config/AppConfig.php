c09e6f9b8646337e73847b72774b93668a25c402
Benjamin Seifert <benjamin.seifert@de.sii.group>
2026-01-28 15:50:56 +0100

    Cleanup

--- core/app/Config/AppConfig.php
+++ core/app/Config/AppConfig.php
@@ -9,21 +9,21 @@ class AppConfig {
     public const ADMIN_VIEWS_PATH = __DIR__ . "/../../views";
     public const VIEW_PARTIALS_PATH = self::VIEWS_PATH . "/partials";
 
-    public static $blog_config = null;
+    public static $user_config = null;
 
     public static function get_user_config(): array {
-        if (self::$blog_config === null) {
+        if (self::$user_config === null) {
             $config_path = __DIR__ . "/../../../config.json";
 
             if (file_exists($config_path)) {
                 $json_content = file_get_contents($config_path);
-                self::$blog_config = json_decode($json_content, true);
+                self::$user_config = json_decode($json_content, true);
             }
             else {
                 die("Configuration file not found at {$config_path}!");
             }
         }
 
-        return self::$blog_config;
+        return self::$user_config;
     }
 }
