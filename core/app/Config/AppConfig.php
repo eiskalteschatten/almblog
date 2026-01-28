82e32fac763dbade41b8cc23442561935d9496e7
Benjamin Seifert <benjamin.seifert@de.sii.group>
2026-01-28 10:22:13 +0100

    Start support for user config

--- core/app/Config/AppConfig.php
+++ core/app/Config/AppConfig.php
@@ -8,8 +8,26 @@ class AppConfig {
     public const VIEWS_PATH = __DIR__ . "/../../views";
     public const VIEW_PARTIALS_PATH = self::VIEWS_PATH . "/partials";
 
+    public static $blogConfig = null;
+
     // Could also add environment-specific configs
     public static function isProduction(): bool {
         return getenv('APP_ENV') === 'production';
+    }
+
+    public static function getUserConfig(): array {
+        if (self::$blogConfig === null) {
+            $configPath = __DIR__ . "/../../../config.json";
+
+            if (file_exists($configPath)) {
+                $jsonContent = file_get_contents($configPath);
+                self::$blogConfig = json_decode($jsonContent, true);
+            }
+            else {
+                self::$blogConfig = [];
+            }
+        }
+
+        return self::$blogConfig;
     }
 }
